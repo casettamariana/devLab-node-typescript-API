a0bcf5758bbf411686f942dadb55bbca01f49e1b
mariana.casetta.az <mariana.casetta@poli.digital>
2022-03-31 17:52:09 -0300

    fix(prettier): fix style

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import "./util/module-alias";
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { ForecastController } from "./controllers/forecast";
-import { Application } from "express";
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-	public init(): void {
-		this.setupExpress();
-    this.setupControllers()
-	}
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
