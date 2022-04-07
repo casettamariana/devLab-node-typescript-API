a020a94297939318deb5db03c050326b4401769a
mariana.casetta.az <mariana.casetta@poli.digital>
2022-04-07 20:09:28 -0300

    refactor(errors): files

--- src/util/errors/clientRequestError.ts
+++ src/util/errors/clientRequestError.ts
@@ -1,4 +1,4 @@
-import { InternalError } from './internalError';
+import { InternalError } from './internal-error';
 
 export class ClientRequestError extends InternalError {
   constructor(message: string) {
--- src/util/errors/stormGlassResponseError.ts
+++ src/util/errors/stormGlassResponseError.ts
@@ -1,4 +1,4 @@
-import { InternalError } from "./internalError";
+import { InternalError } from "./internal-error";
 
 export class StormGlassResponseError extends InternalError {
   constructor(message: string) {
